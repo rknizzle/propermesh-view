3e6809c9727c6c554597d3acb19fe4358b5470a9
Erik Olson <erikamerico1991@gmail.com>
2024-01-31 22:36:36 -0500

    created Geometry and Thickness Analysis sections. responsive to screensizes

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import "./landingPage.css";
+import { Row, Col } from "antd";
 
 export default function Landing() {
   return (
@@ -6,6 +7,79 @@ export default function Landing() {
       <header className="App-header">
         <h1>P R O P E R M E S H</h1>
       </header>
+
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col
+          xs={{ span: 22, offset: 1 }}
+          sm={{ span: 20, offset: 2 }}
+          md={{ span: 16, offset: 4 }}
+          lg={{ span: 8, offset: 3 }}
+          id="geo-analysis"
+        >
+          <h2>Geometry Analysis</h2>
+          <p>
+            Geometry analysis performs detailed geometry analysis on 3D model
+            files. It is designed to efficiently calculate key dimensions and
+            identify potential issues within the 3D mesh.
+          </p>
+          <ul>
+            <li>Surface Area</li>
+            <li>Volume</li>
+            <li>Bounding Box</li>
+            <br />
+            <li>Boundary Edges</li>
+            <li>Number of shells</li>
+          </ul>
+          <br />
+          <br />
+          <div className="use-cases">Use Cases:</div>
+          <ul>
+            <li>
+              Ideal for designers and engineers in pre-production stages to
+              validate 3D models.
+            </li>
+            <li> Useful in 3D printing for ensuring model integrity.</li>
+            <li>
+              Helps in digital archiving of 3D artifacts by providing detailed
+              dimensional data.
+            </li>
+          </ul>
+        </Col>
+        <Col
+          xs={{ span: 22, offset: 1 }}
+          sm={{ span: 20, offset: 2 }}
+          md={{ span: 16, offset: 4 }}
+          lg={{ span: 8, offset: 2 }}
+          id="thick-analysis"
+        >
+          <h2>Thickness Analysis</h2>
+          <p>
+            TODO: Geometry analysis performs detailed geometry analysis on 3D
+            model files. It is designed to efficiently calculate key dimensions
+            and identify potential issues within the 3D mesh.
+          </p>
+          <ul>
+            <li>Percentage of thin surface area</li>
+          </ul>
+          <br />
+          <br />
+          <div className="use-cases">Use Cases:</div>
+          <ul>
+            <li>
+              Essential for 3D printing enthusiasts and professionals to
+              preemptively identify and correct potential printing issues.
+            </li>
+            <li>
+              Useful in product design for ensuring structural integrity and
+              material efficiency.
+            </li>
+            <li>
+              Assists in educational settings for teaching the principles of 3D
+              modeling and printing
+            </li>
+          </ul>
+        </Col>
+      </Row>
     </div>
   );
 }
