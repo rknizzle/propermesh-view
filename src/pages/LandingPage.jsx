712f7bfc32c39ea54620b5f0b6792d840461d581
Erik Olson <erikamerico1991@gmail.com>
2024-02-01 00:27:08 -0500

    cool meshing effect of logo on page load

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,21 @@
+import { useState, useEffect } from "react";
 import "./landingPage.css";
 import { Row, Col } from "antd";
 
 export default function Landing() {
+  const [animateLogo, setAnimateLogo] = useState(false);
+
+  useEffect(() => {
+    setAnimateLogo(true);
+  }, []);
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <h1>P R O P E R M E S H</h1>
-      </header> */}
-
-      <img src="assets/logo.png" alt="" id="logo" />
+      {/* <img src="assets/logo.png" alt="" id="logo" /> */}
+      <div id="logo-container" className={animateLogo ? "logo-animate" : ""}>
+        <div className="logo-half left-half"></div>
+        <div className="logo-half right-half"></div>
+      </div>
 
       <h2>
         Software tools for automating 3D printability checks of 3D model files
