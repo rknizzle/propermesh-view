54c6f6117e929c3933c6df9f769d7f272c60a05c
Erik Olson <erikamerico1991@gmail.com>
2024-03-01 00:58:14 -0500

    dynamically determine if footer should be sticky or not depending on content and viewport height

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,29 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import { useLocation } from "react-router-dom";
 import { AuthProvider } from "./utils/authContext";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const location = useLocation();
-  const isStickyFooterPage =
-    location.pathname === "/loginregister" || location.pathname === "/apppage";
+  const contentRef = useRef(null);
+  const [isStickyFooter, setIsStickyFooter] = useState(false);
+
+  useEffect(() => {
+    const contentHeight = contentRef.current.getBoundingClientRect().height;
+    const viewportHeight = window.innerHeight;
+    // Determine if the footer should be sticky
+    // sets isStickerFooter to true if the content height is less than the viewport height, false otherwise
+    setIsStickyFooter(contentHeight < viewportHeight);
+  }, [location.pathname]); // Rerun this effect if the route changes
+
   return (
     <AuthProvider>
       <>
         <Header />
-        <Outlet />
-        <Footer sticky={isStickyFooterPage} />
+        <div ref={contentRef}>
+          <Outlet />
+        </div>
+        <Footer sticky={isStickyFooter} />
       </>
     </AuthProvider>
   );
