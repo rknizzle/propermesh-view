9416f34965de52af698baf3cfc2b23c9165038c1
Erik Olson <erikamerico1991@gmail.com>
2024-03-11 13:55:57 -0400

    open modal, list uploaded parts when view parts button is clicked

--- src/components/viewPartsButton.jsx
+++ src/components/viewPartsButton.jsx
@@ -1,16 +1,39 @@
-import { Button } from "antd";
+import { useState } from "react";
+import { Button, Modal } from "antd";
 
 const ViewParts = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [partsData, setPartsData] = useState([]);
+
   const clicked = () => {
     console.log("View Parts button clicked");
     fetch("/api/v0/parts")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        setPartsData(data);
+        setModalOpen(true);
+      });
   };
 
   return (
     <>
       <Button onClick={() => clicked()}> View Parts </Button>
+      <Modal
+        title="Parts"
+        open={modalOpen}
+        onOk={() => setModalOpen(false)}
+        onCancel={() => setModalOpen(false)}
+        footer={[
+          <Button key="close" onClick={() => setModalOpen(false)}>
+            Close
+          </Button>,
+        ]}
+      >
+        {partsData.map((part, index) => (
+          <p key={index}>{part.name}</p>
+        ))}
+      </Modal>
     </>
   );
 };
