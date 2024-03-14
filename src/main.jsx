6de9f22b353732276b1dc25c350389e7aa8ffe6c
rknizzle <ryan@autotiv.com>
2024-03-14 11:45:58 -0400

    Rename Register -> RegisterPage

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import AnalysisPage from "./components/AnalysisPage/AnalysisPage.jsx";
 import LoginPage from "./components/loginRegister/LoginPage/LoginPage.jsx";
-import Register from "./components/loginRegister/register/Register.jsx";
+import RegisterPage from "./components/loginRegister/RegisterPage/RegisterPage.jsx";
 
 export const RootComponent = () => {
   return (
@@ -14,7 +14,7 @@ export const RootComponent = () => {
       <Route path="/" element={<App />}>
         <Route index element={<LandingPage />} />
         <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<Register />} />
+        <Route path="register" element={<RegisterPage />} />
         <Route path="analysis" element={<AnalysisPage />} />
       </Route>
     </Routes>
