d6a03f75bd77b32d3a4f7c8880052d2bec58920c
rknizzle <ryan@autotiv.com>
2024-03-14 11:37:55 -0400

    Rename header -> Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { useLocation } from "react-router-dom";
 import { AuthProvider } from "./utils/authContext";
