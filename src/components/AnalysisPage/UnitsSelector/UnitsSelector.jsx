5c475cce0a4e1659108bca41f4a60891414fe580
Erik Olson <erikamerico1991@gmail.com>
2024-03-21 00:13:48 -0400

    update units in db

--- src/components/AnalysisPage/UnitsSelector/UnitsSelector.jsx
+++ src/components/AnalysisPage/UnitsSelector/UnitsSelector.jsx
@@ -1,20 +1,43 @@
 import { useState } from "react";
 import { Select, Space } from "antd";
+import { CheckCircleOutlined } from "@ant-design/icons";
 import "./unitsSelector.css";
 import PropTypes from "prop-types";
 
 const UnitsSelector = ({ partId }) => {
   const [units, setUnits] = useState(undefined);
-  console.log("partId: ", partId);
+  const [showSuccess, setShowSuccess] = useState(false);
 
-  const handleChange = (value) => {
+  const handleChange = async (value) => {
     setUnits(value);
-    console.log(`selected ${value}`);
+    try {
+      const response = await fetch(`/api/v0/parts/${partId}/units`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ units: value }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update units.");
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 2000);
+    } catch (error) {
+      console.error("Error updating units:", error);
+    }
   };
 
   return (
-    <div>
-      <Space wrap>
+    <Space wrap>
+      <div id="select-container">
         <span id="units-selector-label">Units: </span>
         <Select
           placeholder="Select units"
@@ -26,8 +49,14 @@ const UnitsSelector = ({ partId }) => {
             { value: "inches", label: "inches" },
           ]}
         />
-      </Space>
-    </div>
+        {showSuccess && (
+          <CheckCircleOutlined
+            style={{ color: "green", fontSize: "16px" }}
+            id="unit-update-success-icon"
+          />
+        )}
+      </div>
+    </Space>
   );
 };
 
