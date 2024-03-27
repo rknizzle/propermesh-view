30cd622a9df0627825abf0ec81fefb49663eae54
Erik Olson <erikamerico1991@gmail.com>
2024-03-26 23:04:18 -0400

    simplify updatePartUnit to not throw redundant console.error messages

--- src/components/AnalysisPage/UnitsSelector/updatePartUnits.js
+++ src/components/AnalysisPage/UnitsSelector/updatePartUnits.js
@@ -1,21 +1,16 @@
 export const updatePartUnits = async (partId, units) => {
-  try {
-    const response = await fetch(`/api/v0/parts/${partId}/units`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ units }),
-    });
+  const response = await fetch(`/api/v0/parts/${partId}/units`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ units }),
+  });
 
-    if (!response.ok) {
-      throw new Error("Failed to update units.");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error updating units:", error);
-    throw error;
+  if (!response.ok) {
+    throw new Error("Failed to update units.");
   }
+
+  const data = await response.json();
+  return data;
 };
