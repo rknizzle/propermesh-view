8b66e2a607f00fab2f124a082ea762be00db5007
Erik Olson <erikamerico1991@gmail.com>
2024-03-29 21:59:41 -0400

    show checkmark on succesful analysis

--- src/components/AnalysisPage/GeoAnalysisBox/StartButton/StartButton.jsx
+++ src/components/AnalysisPage/GeoAnalysisBox/StartButton/StartButton.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button, notification, Tooltip, Spin } from "antd";
+import { CheckCircleOutlined } from "@ant-design/icons";
 import { startGeometryAnalysis } from "./startGeoAnalysis";
 import { pollForResults } from "./pollForResults";
 import PropTypes from "prop-types";
@@ -7,13 +8,17 @@ import "./startButton.css";
 
 const StartButton = ({ partId, setGeoData }) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [showCheckmark, setShowCheckmark] = useState(false);
 
   const startAnalysis = async () => {
     setIsLoading(true);
+    setShowCheckmark(false);
     try {
       const job = await startGeometryAnalysis(partId);
 
       await pollForResults(job.id, setIsLoading, setGeoData);
+      setShowCheckmark(true);
+      setTimeout(() => setShowCheckmark(false), 5000);
     } catch (error) {
       notification.error({
         message: "Analysis Failed",
@@ -39,6 +44,12 @@ const StartButton = ({ partId, setGeoData }) => {
         {isLoading ? "Analyzing..." : "Start"}
         {isLoading ? <Spin style={{ marginLeft: "10px" }} /> : null}
       </Button>
+      {showCheckmark ? (
+        <CheckCircleOutlined
+          style={{ marginLeft: "10px" }}
+          id="checkmark-icon"
+        />
+      ) : null}
     </Tooltip>
   );
 };
