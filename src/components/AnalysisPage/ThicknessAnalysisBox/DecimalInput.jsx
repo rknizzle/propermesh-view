738a474ba8a44bf630cae1d40481ffeb12c20e0f
Erik Olson <erikamerico1991@gmail.com>
2024-04-07 23:44:29 -0400

    Fix the thresholdValue  NaN bug, allow a user to start their input with a decimal point

--- src/components/AnalysisPage/ThicknessAnalysisBox/DecimalInput.jsx
+++ src/components/AnalysisPage/ThicknessAnalysisBox/DecimalInput.jsx
@@ -3,7 +3,11 @@ import { Input } from "antd";
 import PropTypes from "prop-types";
 
 const DecimalInput = ({ value, onChange }) => {
-  const [inputValue, setInputValue] = useState(true);
+  const [inputValue, setInputValue] = useState("");
+
+  // The inputValue controls what is displayed in the input field
+  // allowing us to see values like ".0" or "."
+  // once the user enters a valid number, the value will be updated as well as thresholdValue
 
   useEffect(() => {
     setInputValue(value?.toString().trim() ?? "");
@@ -14,10 +18,22 @@ const DecimalInput = ({ value, onChange }) => {
     const newValue = e.target.value.trim();
     const reg = /^(\d+)?(\.\d{0,2})?$/;
 
-    if (newValue === "" || reg.test(newValue)) {
+    if (reg.test(newValue)) {
       setInputValue(newValue);
-      const numericValue = newValue === "" ? null : parseFloat(newValue);
-      onChange(numericValue);
+
+      // checking to see if newValue is either ".", ".0" or if it's a number that passes the regex test
+      // if newValue is ".", ".0" or "" set numericValue to null
+      // allows the user to keep entering their value until they enter a valid number
+      // parseFloat won't be dealing with something that is not a number
+      // if newValue is a valid number, convert it to float representation
+      if (newValue === "." || newValue === ".0" || reg.test(newValue)) {
+        // || reg.test(newValue) seems redundant here, but the input isn't recognized as a number without it
+        const numericValue =
+          newValue === "." || newValue === ".0" || newValue === ""
+            ? null
+            : parseFloat(newValue);
+        onChange(numericValue);
+      }
     }
   };
 
