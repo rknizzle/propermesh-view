2c85b5d1f523a861bd1d967d34703b68104310e3
Erik Olson <erikamerico1991@gmail.com>
2024-04-26 00:23:52 -0400

    use AuthProvider in main.jsx instead of App.jsx, use ProtectedRoute for Analysis Page route. cleanup the structure of main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { AuthProvider } from "./utils/authContext";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -7,22 +8,23 @@ import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import AnalysisPage from "./components/AnalysisPage/AnalysisPage.jsx";
 import LoginPage from "./components/loginRegister/LoginPage/LoginPage.jsx";
 import RegisterPage from "./components/loginRegister/RegisterPage/RegisterPage.jsx";
+import ProtectedRoute from "./utils/protectedRoute.jsx";
 
-export const RootComponent = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<LandingPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="analysis" element={<AnalysisPage />} />
-      </Route>
-    </Routes>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <RootComponent />
-  </Router>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<LandingPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route
+            path="analysis"
+            element={<ProtectedRoute component={AnalysisPage} />}
+          />
+        </Route>
+      </Routes>
+    </Router>
+  </AuthProvider>
 );
